b47b58cdd0173b756fb8d9dcec587b80017a002f
seada98 <mohamedseada1998@gmail.com>
2023-01-26 00:29:53 +0200

    Edit SA

--- Terraform/service-account.tf
+++ Terraform/service-account.tf
@@ -1,3 +1,10 @@
+resource "google_project_iam_custom_role" "my-custom-role" {
+  role_id     = "myCustomRole"
+  title       = "My Custom Role"
+  description = "A description"
+  permissions = ["resourcemanager.projects.get", "storage.buckets.get", "storage.buckets.list" , "storage.objects.get" , "storage.objects.list" , "container.deployments.get" , "container.deployments.create" , "container.deployments.list" , "container.services.list" , "container.services.get" , "container.services.create" , "container.clusters.list" , "container.clusters.getCredentials" , "container.clusters.get" , "container.pods.list" ,"container.nodes.list" , ]
+}
+
 resource "google_service_account" "project-service-account" {
   account_id = "project-service-account"
   project = "iti-seada"
@@ -8,5 +15,12 @@ resource "google_project_iam_binding" "project-service-account-iam" {
   role    = "roles/storage.admin"
   members = [
   "serviceAccount:${google_service_account.project-service-account.email}"
+  ]
+}
+resource "google_project_iam_binding" "project-service-account-iam2" {
+  project = "iti-seada"
+  role    = "projects/${google_service_account.project-service-account.project}/roles/${google_project_iam_custom_role.my-custom-role.role_id}"
+  members = [
+    "serviceAccount:${google_service_account.project-service-account.email}"
   ]
 }
